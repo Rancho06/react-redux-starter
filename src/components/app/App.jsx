0f6b16816ddd0a61d059021600a128d5851950a8
Shu Zhou <shu.zhou@salesforce.com>
2020-02-16 15:23:23 -0800

    add initial css

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,10 @@
 // @flow
+import React from 'react';
+
+import 'src/components/app/App.css';
+
 type Props = {||};
 
 export default function(props: Props) {
-	return 'This is a React starter template built on top of create-react-app';
+	return <div>This is a React starter template built on top of create-react-app</div>;
 }
